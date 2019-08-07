f43566c745dc23669f475283b06f70cf950c79a1
Václav Němec <vaclavnemec@email.cz>
2019-08-07 16:47:24 +0200

    javadoc for the loan service

--- src/main/java/com/example/demo/LoanService.java
+++ src/main/java/com/example/demo/LoanService.java
@@ -3,5 +3,10 @@ package com.example.demo;
 import java.util.List;
 
 public interface LoanService {
+
+    /**
+     * @param millis only the loans published between "now" and "now - amount of milliseconds" will be returned
+     * @return new loans
+     */
     List<Loan> getRecentLoans(int millis);
 }
